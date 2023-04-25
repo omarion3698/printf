c0ff1cfdee4f9d236307fa25ccf4662612468826
saaid <saaid2538@gmail.com>
2023-04-24 23:11:36 -0700

    added main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int main(void)
+{
+	int len, len2;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Negative:[%d]\n", -762534);
+	return (0);
+}
