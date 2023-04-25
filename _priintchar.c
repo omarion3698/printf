e6ab1f097422e2eff50aef7c1a966b1e180cd2a9
saaid <saaid2538@gmail.com>
2023-04-24 22:54:03 -0700

    added _priintchar.c

---
+++ _priintchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printchar - Prints a character to stdout.
+ * @c: The character to print.
+ * Return: Always 1.
+ */
+int _printchar(char c)
+{
+	return (_putchar(c));
+}
